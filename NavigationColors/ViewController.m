911cdd65e92d2f03b9bebaf0c3c8b3cf17e0bbd9
Taylor Mott <trm36@humboldt.edu>
2014-09-18 20:09:43 -0600

    Complete
    - Add a method “color” that changes the background color to blue then back to white when called
    - Initialize a barButtonItem with self as target and color: as the selector
    - Set the barButtonItem to the navigationItem's rightBarButtonItem
    
    Signed-off-by: Taylor Mott <trm36@humboldt.edu>

--- NavigationColors/ViewController.m
+++ NavigationColors/ViewController.m
@@ -17,6 +17,21 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    UIBarButtonItem *rightButton = [[UIBarButtonItem alloc] initWithTitle:@"Button" style:UIBarButtonItemStylePlain target:self action:@selector(color)];
+    self.navigationItem.rightBarButtonItem = rightButton;
+    self.view.backgroundColor = [UIColor whiteColor];
+}
+
+- (void)color
+{
+    if (self.view.backgroundColor == [UIColor whiteColor])
+    {
+        self.view.backgroundColor = [UIColor blueColor];
+    }
+    else
+    {
+        self.view.backgroundColor = [UIColor whiteColor];
+    }
 }
 
 - (void)didReceiveMemoryWarning {
